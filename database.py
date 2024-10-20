682e03d5ba40e4e07568da06a7235ff2ea9ca863
Daniel Legaz <dlal2@gcloud.ua.es>
2024-10-20 13:22:41 +0200

    bbdd

--- database.py
+++ database.py
@@ -64,7 +64,6 @@ cursor.execute('''
     ('e', 50, 50)
 ''')
 
-"""
 
 cursor.execute('''
     INSERT INTO destinos (destino, coordX, coordY) VALUES
@@ -76,14 +75,18 @@ cursor.execute('''
     ('T', 18, 3)
 ''')
 
+"""
+
+
 cursor.execute('''
     INSERT INTO taxis (id, destino_a_cliente, destino_a_final, estado, coordX, coordY) VALUES
-    (1, 'a', 'N', 0, 1, 1),
-    (2, 'b', 'X', 0, 1, 1),
-    (3, 'c', 'U', 0, 1, 1),
-    (4, 'd', 'L', 0, 1, 1),
-    (5, 'e', 'T', 0, 1, 1)
+    (2, 'b', 'X', 1, 1, 1),
+    (3, 'c', 'U', 1, 1, 1),
+    (4, 'd', 'L', 1, 1, 1),
+    (5, 'e', 'T', 1, 1, 1)
 ''')
+
+
 
 #================================================================================================
 
